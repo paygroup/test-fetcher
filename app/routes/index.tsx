1d72025d82e993326e6ab5318b2121eff155957d
seyaobey-dev <seyaobey.dev@gmail.com>
2023-03-29 23:35:41 +0100

    clear content

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 import { ActionFunction, json } from "@remix-run/node";
-import { Form, useActionData, useFetcher } from "@remix-run/react";
+import { useActionData, useFetcher } from "@remix-run/react";
 
 export const action: ActionFunction = async () => {
   console.log("action called");
@@ -14,37 +14,11 @@ export default function Index() {
   console.log("action", action);
 
   return (
-<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
 
       <button onClick={() => {
         fetcher.submit({});
-      }} >action</button>
+      }} >click me</button>
       
     </div>
   );
